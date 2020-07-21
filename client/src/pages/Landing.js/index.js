cade5ee964c1d13bbac568bb731ad1e4022f07aa
Joshua Benjamin <jbenj@sas.upenn.edu>
2020-07-21 17:35:06 -0400

    landing page initiated, images added

---
+++ client/src/pages/Landing.js/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+    state = {
+        newGoals,
+        yourGoals,
+        completedGoals
+    }
+}
+
+export default Landing;
