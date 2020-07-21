9d15c237d3e6683fa4d540da57515a78dcbeb8c3
Brian Levin <bml201095@gmail.com>
2020-07-21 12:38:26 -0400

    made some corrections on bootstrap page

--- client/src/pages/newgoal.js
+++ client/src/pages/newgoal.js
@@ -2,50 +2,47 @@
 import React from 'react';
 
 
-class NameForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { value: '' };
+class NewGoal extends React.Component {
+    constructor(props) { // as soon as component is rendered
+        super(props); // calls the parent react component class structure
+        this.state = { value: '' }; // intializing state data is being in compoent so its intializing it
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+      
     }
 
-    handleChange(event) {
-        this.setState({ value: event.target.value });
-    }
+    
 
-    handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
-    }
+   
+    
 
-    render() {
+    render() { // renders bootstrap html
         return (
             <form>
                 <div className="form-group">
-                    <label for="formGroupExampleInput">Title of new goal</label>
-                    <input type="text" className="form-control" id="formGroupExampleInput" placeholder="New Goal"/>
+                    <label forHTML="formGroupExampleInput">Title of new goal</label>
+                    <input type="text" className="form-control" id="formGroupExampleInput" placeholder="New Goal" />
                     <div className="form-group">
-    <label for="exampleFormControlSelect2">categories</label>
-    <select multiple className="form-control" id="exampleFormControlSelect2">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </select>
-  </div>
-</div>
+                        <label for="exampleFormControlSelect2">categories</label>
+                        <select multiple className="form-control" id="exampleFormControlSelect2">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                </div>
 
- <div class="form-group">
- <label for="exampleFormControlTextarea1">Description</label>
- <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"/>
-</div>
-<button type="submit" class="btn btn-primary">Create new goal</button>
+                <div className="form-group">
+                    <label forHTLM="exampleFormControlTextarea1">Description</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" />
+                </div>
+                <button type="submit" className="btn btn-primary">Create new goal</button>
 
-</form>
+            </form>
 
-                );
-                    }
-  }
+        );
+    }
+}
+
+export default NewGoal;
