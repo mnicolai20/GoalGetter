8ee581f5a07fd8fb224284c13a2f1a525ead6d3c
nicolai <mattrnicolai@gmail.com>
2020-07-23 19:23:48 -0400

    added functions to signup.js and set up login.js

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -51,11 +51,8 @@ render() {
         <h4>Sign up</h4>
         <Form>
             <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-                </Form.Text>
+                <Form.Label>Create Username</Form.Label>
+                <Form.Control type="input" placeholder="Enter username" />
             </Form.Group>
 
             <Form.Group controlId="formBasicPassword">
@@ -65,7 +62,7 @@ render() {
             <Form.Group controlId="formBasicCheckbox">
                 <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
-          <Button variant="primary" type="submit">
+          <Button variant="primary" type="submit" onClick={this.handleSubmit}>
             Submit
           </Button>
         </Form>
@@ -73,3 +70,5 @@ render() {
     )
 }
 }
+
+export default Signup;
