c35768b6177639caac0b2f144fd972065aaf5cc2
Brandon P <brandonj.peart1@gmail.com>
2020-07-25 10:10:13 -0400

    updated files

--- models/goals.js
+++ models/goals.js
@@ -2,11 +2,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const newGoal = new Schema({
-    title: { type: String, required: true },
-    category: { type: Selection, required: true },
-    description: { type: String, required: true },
+    title: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
 });
 
-const User = mongoose.model("newGoal", newGoalSchema);
+const newGoal = mongoose.model("newGoal", newGoalSchema);
 
 module.exports = newGoal;
