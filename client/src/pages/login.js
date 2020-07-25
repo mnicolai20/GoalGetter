32028e1d555a6ae4af3706e529f984497856151e
nicolai <mattrnicolai@gmail.com>
2020-07-25 10:14:20 -0400

    defined button

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
-import axios from "axios";
 import { Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import axios from "axios";
 
 
 class Login extends Component {
@@ -58,7 +58,7 @@ class Login extends Component {
 
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" value={this.state.password} onChange={this.handleChange}/>
+                        <Form.Control type="input" placeholder="Password" value={this.state.password} onChange={this.handleChange}/>
                     </Form.Group>
                     <Button variant="primary" type="submit" onClick={this.handleSubmit}>
                         Submit
@@ -68,3 +68,5 @@ class Login extends Component {
         }
     }
 }
+
+export default Login;
--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import axios from "axios";
 
 class Signup extends Component {
@@ -57,7 +58,7 @@ render() {
 
             <Form.Group controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" value={this.state.password} onChange={this.handleChange}/>
+                <Form.Control type="input" placeholder="Password" value={this.state.password} onChange={this.handleChange}/>
             </Form.Group>
             <Button variant="primary" type="submit" onClick={this.handleSubmit}>
                 Submit
