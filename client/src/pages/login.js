a6a7eecb2545f6d32b40eda83f4dca89877914a5
nicolai <mattrnicolai@gmail.com>
2020-07-26 00:30:35 -0400

    signup and login forms now functioning as components that render onto page

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,78 +1,11 @@
+import LoginForm from "../components/LoginForm"
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
-import axios from "axios";
-import { Form } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
-import "./login.css";
 
-class Login extends Component {
-    constructor() {
-        super()
-        this.state = {
-            username: "",
-            password: "",
-            redirectTo: null
-        }
-        this.handleSubmit = this.handleSubmit.bind(this)
-        this.handleChange = this.handleChange.bind(this)
-    }
-    
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
-
-    handleSubmit(event) {
-        event.preventDefault()
-
-        axios.post("/user/login", {
-            username: this.state.username,
-            password: this.state.password
-        })
-        .then(response => {
-            console.log(response)
-            if(response.status === 200) {
-                this.props.updateUser({
-                    loggedIn: true,
-                    username: response.data.username
-                })
-                this.setState({
-                    redirectTo: "/"
-                })
-            }
-        }).catch(error => {
-            console.log(error);
-        })
-    }
-
+class Login extends Component{
     render() {
-        if(this.state.redirectTo){
-            return <Redirect to={{ pathname: this.state.redirectTo }} />
-        } else {
-            return (
-                <div className="page">
-                <h4 className="title">Log In</h4>
-                <Form>
-                    <Form.Group controlId="formBasicEmail" className="usernameLine">
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control type="input" placeholder="Enter username" />
-                    </Form.Group>
-
-                    <Form.Group controlId="formBasicPassword" className="usernameLine">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="input" placeholder="Password" value={this.state.password} onChange={this.handleChange}/>
-                    </Form.Group>
-                    <Button variant="primary" type="submit" onClick={this.handleSubmit} className="submit">
-                        Submit
-                    </Button>
-                    <Button variant="primary" type="submit" href="/signup" className="submit">
-                        Sign Up!
-                    </Button>
-                </Form>
-                </div>
-            )
-        }
+        return(
+            <LoginForm />
+        )
     }
 }
 
--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,73 +1,12 @@
+import SignupForm from "../components/SignupForm";
 import React, { Component } from "react";
-import { Form } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
-import axios from "axios";
-import "./login.css";
 
 class Signup extends Component {
-    constructor() {
-        super()
-        this.state = {
-            username: "",
-            password: "",
-            confirmPassword: ""
-        }
-        this.handleSubmit = this.handleSubmit.bind(this)
-        this.handleChange = this.handleChange.bind(this)    
-    };
-
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    };
-
-    handleSubmit(event) {
-        event.preventDefault();
-
-        axios.post("/user/", {
-            username: this.state.username,
-            password: this.state.password
-        })
-        .then(response => {
-            console.log(response)
-            if(!response.data.errmsg){
-                console.log("successful signup")
-                this.getSnapshotBeforeUpdate({
-                    // direct to login page
-                    redirectTo: "/login"
-                })
-            } else{
-                console.log("username already taken")
-            }
-        }).catch(error => {
-            console.log("signup error")
-            console.log(error);
-        })
+    render(){
+        return(
+            <SignupForm />
+        )
     }
-
-
-render() {
-    return(
-    <div className="page">
-        <h4 className="title">Sign up</h4>
-        <Form className="signupSheet">
-            <Form.Group controlId="formBasicEmail" className="usernameLine">
-                <Form.Label>Create Username</Form.Label>
-                <Form.Control type="input" placeholder="Enter username"/>
-            </Form.Group>
-
-            <Form.Group controlId="formBasicPassword" className="usernameLine">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="input" placeholder="Password" value={this.state.password} onChange={this.handleChange}/>
-            </Form.Group>
-            <Button variant="primary" type="submit" onClick={this.handleSubmit}>
-                Submit
-            </Button>
-        </Form>
-    </div>
-    )
-}
 }
 
 export default Signup;
