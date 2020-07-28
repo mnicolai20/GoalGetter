ec2e6ece3cbcb333c5488269f1be3a059736d7f9
Joshua Benjamin <jbenj@sas.upenn.edu>
2020-07-28 13:12:20 -0400

    work on homepage styling and buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NewGoals from "./pages/newgoal";
 import SignUp from "./pages/signup";
 import LogIn from "./pages/login";
+import Homepage from "./pages/homepage";
 ;
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
         <Route exact path="/" component={LogIn} />
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/newgoals" component={NewGoals} />
+        <Route exact path="/homepage" component={Homepage} />
 
       </div>
     </Router>
