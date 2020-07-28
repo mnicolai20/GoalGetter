92c7242200aa229330226732e6c5027dc23c6a46
Brian Levin <bml201095@gmail.com>
2020-07-28 12:32:07 -0400

    updated goal in front end router

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import NewGoals from "./pages/newgoal";
 import SignUp from "./pages/signup";
 import LogIn from "./pages/login";
+import UpdateGoal from "./pages/updategoal"
 ;
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" component={LogIn} />
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/newgoals" component={NewGoals} />
+        <Route exact path="/updategoal" component={UpdateGoal} />
 
       </div>
     </Router>
