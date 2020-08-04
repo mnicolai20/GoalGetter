db8f5a84a3a09ced6bda1a5e5709b04d5a2f6c94
Joshua Benjamin <jbenj@sas.upenn.edu>
2020-08-03 22:05:17 -0400

    newgoal file work

--- controller/goalsController.js
+++ controller/goalsController.js
@@ -15,11 +15,15 @@ module.exports = {
     },
     create: function (req, res) {
         console.log('Here')
-
+        console.log(typeof req.body)
         //
         db.Goals
             .create(req.body)
-            .then(dbModel => res.json(dbModel))
+            .then(dbModel =>{
+                console.log("response obj")
+                console.log(dbModel)
+                res.json(dbModel)
+            })
             .catch(err => res.status(422).json(err));
     },
     update: function (req, res) {
