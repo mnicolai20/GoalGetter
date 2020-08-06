40c99aafbbb4500831756cba6707c7a9709904b1
nicolai <mattrnicolai@gmail.com>
2020-08-06 14:14:17 -0400

    edited nav component

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link className="navbar-brand" to="/homepage">
+        Homepage
+      </Link>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/newgoals"
+              className={
+                window.location.pathname === "/homepage" || window.location.pathname === "/newgoals"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Create a Goal
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
